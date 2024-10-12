d22d79207be93de2fd9f30b942f8ec5568696018
Spark2000 <paulalex.wetzel@gmail.com>
2024-10-12 09:51:57 +0200

    add: fasterSwimming

--- addons/RB205_gameplayChanges/fasterSwimming/config.cpp
+++ addons/RB205_gameplayChanges/fasterSwimming/config.cpp
@@ -1,30 +1,54 @@
+/*
+	Speeds Up the swimming animations and changes every uniform into a neopren suit
+*/
+
 class cfgPatches
 {
 	class RB205_gameplayChanges_fasterSwimming
 	{
 		requiredAddons[] =
         {
+			"A3_Characters_F",
             "A3_Anims_F_Config_Sdr"
         };
 		requiredVersion = 1.0;
 		units[] = {};
-		weapons[] = {};
+		weapons[] = 
+		{
+			"Uniform_Base"
+		};
 	};
 };
 
-/*
-	Speeds Up the swimming animations and changes every uniform into a neopren suit
-*/
+class CfgWeapons
+{
+	class InventoryItem_Base_F;
+	class UniformItem: InventoryItem_Base_F
+	{
+		type = 801;
+		uniformType = "Neopren";
+	};
+	class ItemCore;
+	class Uniform_Base: ItemCore
+	{
+		scope = 0;
+		allowedSlots[] = {901};
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "B_Soldier_F";
+			containerClass = "Supply0";
+			uniformType = "Neopren";
+			mass = 0;
+		};
+	};
+};
 
 class CfgMovesBasic;
 class CfgMovesMaleSdr: CfgMovesBasic
 {
 	class States
 	{
-		//speed = 0.14336; - Standard
-		//speed = 0.25     -
-		//speed = 0.3;     - Zu Schnell
-
 		class AdvePercMrunSnonWnonDf;
 		class AdvePercMsprSnonWnonDf: AdvePercMrunSnonWnonDf
 		{
