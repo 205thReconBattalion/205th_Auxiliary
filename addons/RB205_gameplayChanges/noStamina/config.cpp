2db2cece3528c1540dc6e135c6f7abb7b6b54d89
Spark2000 <paulalex.wetzel@gmail.com>
2024-10-12 08:02:53 +0200

    add: noStamina

---
+++ addons/RB205_gameplayChanges/noStamina/config.cpp
@@ -0,0 +1,48 @@
+class cfgPatches
+{
+	class RB205_gameplayChanges_noStamina
+	{
+		requiredAddons[] =
+        {
+            "ace_advanced_fatigue"
+        };
+		requiredVersion = 1.0;
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+/*
+	Deactivates Stamina (vanilla and ace)
+*/
+
+class Extended_PreStart_EventHandlers
+{
+	class ace_advanced_fatigue
+	{
+		init="";
+	};
+};
+class Extended_PreInit_EventHandlers
+{
+    noStamina_init="noStamina_init_var = [] execVM ""\RB205_gameplayChanges\noStamina\init.sqf""";
+    class ace_advanced_fatigue
+	{
+		init="";
+	};
+};
+
+class Extended_PostInit_EventHandlers
+{
+	class ace_advanced_fatigue
+	{
+		init="";
+	};
+};
+class Extended_DisplayLoad_EventHandlers
+{
+	class RscDisplayMission
+	{
+		ace_advanced_fatigue="";
+	};
+}
