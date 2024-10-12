77873cf7533e07e9e565359768603f5ed1745eb3
Spark2000 <paulalex.wetzel@gmail.com>
2024-10-12 08:26:14 +0200

    add: debugConsoleForAdmin

---
+++ addons/RB205_gameplayChanges/debugConsoleForAdmin/config.cpp
@@ -0,0 +1,16 @@
+class cfgPatches
+{
+	class RB205_gameplayChanges_debugConsoleForAdmin
+	{
+		requiredAddons[] = {};
+		requiredVersion = 1.0;
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+/*
+	Enables the debug console for logged in admins
+*/
+
+enableDebugConsole = 1;
