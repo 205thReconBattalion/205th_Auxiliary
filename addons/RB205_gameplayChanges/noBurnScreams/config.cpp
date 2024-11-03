3136b373f291b2d1770f93cacf93411477749f5b
Spark2000 <paulalex.wetzel@gmail.com>
2024-11-03 20:48:01 +0100

    noBurnScreams

---
+++ addons/RB205_gameplayChanges/noBurnScreams/config.cpp
@@ -0,0 +1,167 @@
+/*
+	Deactivates the screams of burning units
+*/
+
+class cfgPatches
+{
+	class RB205_gameplayChanges_noBurnScreams
+	{
+		requiredAddons[] =
+        {
+            "WBK_PhoenixTreal_FlamethrowerMOD"
+        };
+        skipWhenMissingDependencies = 1;
+		requiredVersion = 1.0;
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+class CfgSounds
+{
+	sounds[] = {};
+	class hit_by_flame_1
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class hit_by_flame_2
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class hit_by_flame_3
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class hit_by_flame_4
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath_distant1
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath_distant2
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath_distant3
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath_distant4
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath_distant5
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath_distant6
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath1
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath2
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath3
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath4
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath5
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+	class burnttodeath6
+	{
+		sound[]=
+		{
+			"RB205_gameplayChanges\noJetpackFalldamage\Blank_Sound.ogg",
+			0,
+			0
+		};
+	};
+};
