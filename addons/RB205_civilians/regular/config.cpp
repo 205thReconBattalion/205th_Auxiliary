ca6acf4055140c6f929810a6f6071f2fb1f4dc2d
Spark2000 <paulalex.wetzel@gmail.com>
2024-11-06 00:30:58 +0100

    update: regularCivilians

--- addons/RB205_civilians/regular/config.cpp
+++ addons/RB205_civilians/regular/config.cpp
@@ -1,12 +1,14 @@
 class cfgPatches
 {
-	class RB205_civilians_scientists
+	class RB205_civilians_regular
 	{
 		author = "205th Recon Battalion";
 		requiredAddons[] =
 		{
             "RB205_civilians",
-			"JMSLLTE_civs_mod"
+			"JMSLLTE_civs_mod",
+            "JMSLLTE_scum_mod",
+            "JMSLLTE_empire_mod"
 		};
         skipWhenMissingDependencies = 1;
 		requiredVersion = 1.0;
@@ -18,16 +20,16 @@ class cfgPatches
             "RB205_civ_citizen4",
             "RB205_civ_worker1",
             "RB205_civ_worker2",
-            "RB205_civ_worker3"
+            "RB205_civ_worker3",
+            "RB205_civ_smuggler1",
+            "RB205_civ_smuggler2",
+            "RB205_civ_prisoner1",
+            "RB205_civ_prisoner1_collar"
 		};
 	};
 };
 
 #include "\RB205_civilians\macros.hpp"
-
-//Worker
-//Smuggler
-//Prisoner
 
 #define ITEMS_CITIZEN {"JLTS_ids_rep_civ","JLTS_credit_card","JLTS_intel_datapad_civ"}
 #define ITEMS_SMUGGLER {"JLTS_ids_rep_civ","JLTS_credit_card","JLTS_intel_datapad_civ","JLTS_credits_100","JLTS_credits_50","JLTS_credits_10","JLTS_sabacc_deck"}
@@ -119,5 +121,48 @@ class cfgVehicles
         Items[] = ITEMS_CITIZEN;
         linkedItems[] = {"ItemMap","ItemCompass","ItemWatch","ItemRadio","JMSLLTE_c_VestPilot_black"};
         respawnLinkedItems[] = {"ItemMap","ItemCompass","ItemWatch","ItemRadio","JMSLLTE_c_VestPilot_black"};
+    };
+
+    class RB205_civ_smuggler1: RB205_civ_citizen1 //JMSLLTE_ScumPilotJack_beige_F
+    {
+        displayName = "Smuggler 1";
+        
+        uniformClass = "JMSLLTE_ScumPilotJack_beige_F_CombatUniform";
+        
+        identityTypes[] = {IDENTITY_TYPES_HUMAN,IDENTITY_TYPES_ALIEN};
+        Items[] = ITEMS_SMUGGLER;
+        linkedItems[] = {"ItemMap","ItemCompass","ItemWatch","ItemRadio"};
+        respawnLinkedItems[] = {"ItemMap","ItemCompass","ItemWatch","ItemRadio"};
+    };
+    class RB205_civ_smuggler2: RB205_civ_citizen1
+    {
+        displayName = "Smuggler 2";
+        
+        uniformClass = "JMSLLTE_ScumPilotJack_blue_F_CombatUniform";
+        
+        identityTypes[] = {IDENTITY_TYPES_HUMAN,IDENTITY_TYPES_ALIEN};
+        Items[] = ITEMS_SMUGGLER;
+        linkedItems[] = {"ItemMap","ItemCompass","ItemWatch","ItemRadio"};
+        respawnLinkedItems[] = {"ItemMap","ItemCompass","ItemWatch","ItemRadio"};
+    };
+
+    class RB205_civ_prisoner1: RB205_civ_citizen1
+    {
+        displayName = "Prisoner";
+        editorSubcategory = "RB205_civ_poi";
+        
+        uniformClass = "JMSLLTE_EmpNavyCrew_red_F_CombatUniform";
+        
+        identityTypes[] = {IDENTITY_TYPES_HUMAN,IDENTITY_TYPES_ALIEN};
+        Items[] = ITEMS_PRISONER;
+        linkedItems[] = {};
+        respawnLinkedItems[] = {};
+    };
+    class RB205_civ_prisoner1_collar: RB205_civ_prisoner1
+    {
+        displayName = "Prisoner (Collar)";
+
+        linkedItems[] = {"JMSLLTE_JinataTranslatorNV"};
+        respawnLinkedItems[] = {"JMSLLTE_JinataTranslatorNV"};
     };
 };
