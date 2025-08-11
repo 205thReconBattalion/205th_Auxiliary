0621dd608722858298ec7fc6eeccca9cadeca35b
Spark2000 <paulalex.wetzel@gmail.com>
2025-08-11 13:59:47 +0200

    fix: VR World Grass

---
+++ addons/modCompats/717/vrWorld/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+	class modCompats_717_vrWorld
+	{
+		requiredAddons[] =
+		{
+			"P_Orto_Optional"
+		};
+        skipWhenMissingDependencies = 1;
+		requiredVersion = 1.0;
+        author = "205th Recon Battalion";
+		units[] = {};
+	};
+};
+
+class CfgWorlds
+{
+	class CAWorld;
+	class VR: CAWorld
+	{
+		midDetailTexture = "A3\Map_VR\data\L_middle_mco.paa";
+	};
+};
