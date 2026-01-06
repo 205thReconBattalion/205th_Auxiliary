9861de4415e72c1cf10de9549d7856426cfbad31
Spark <paulalex.wetzel@gmail.com>
2026-01-06 19:58:36 +0100

    update: joinButton

--- addons/RB205_gameplayChanges/quickJoin/config.cpp
+++ addons/RB205_gameplayChanges/quickJoin/config.cpp
@@ -38,7 +38,7 @@ class RscDisplayMain: RscStandardDisplay
 					text="Join 205th (MainOp)"; // text="205th Server I";
 					tooltip="Server beitreten (hier findet statt: MainOp)"; // tooltip="Hier findet statt: MainOP, FunOP, Training";
 					y="(3 * 	1.5) * 	(pixelH * pixelGrid * 2) + 	(pixelH)";
-					onbuttonclick="connectToServer ['5.9.255.45', 2900, '205']";
+					onbuttonclick="connectToServer ['45.152.241.67', 2302, '205']";
 				};
 				class Join205Server_FunOp1: ServerBrowser
 				{
@@ -46,7 +46,7 @@ class RscDisplayMain: RscStandardDisplay
 					text="Join 205th (FunOp I)";
 					tooltip="Server beitreten (hier findet statt: FunOp)";
 					y="(4 * 	1.5) * 	(pixelH * pixelGrid * 2) + 	(pixelH)";
-					onbuttonclick="connectToServer ['5.9.255.45', 2903, '205']";
+					onbuttonclick="connectToServer ['45.152.241.67', 2306, '205']";
 				};
 				class Join205Server_FunOp2: ServerBrowser
 				{
